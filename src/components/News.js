8d421f825d48cd8137e87830cda46bf3e3884282
sdshan8 <sdshan8@gmail.com>
2023-09-04 14:44:30 +0530

    Changes to be committed: 	new file:   sample.json 	modified:   src/components/News.js 	modified:   src/components/Newsitems.js

--- src/components/News.js
+++ src/components/News.js
@@ -4,9 +4,22 @@ import Newsitems from './Newsitems'
 export class News extends Component {
   render() {
     return (
-      <div>
-        news
-        <Newsitems/>
+      <div className='container my-3'>
+        <h1>NewsMonkey - Top Headlines</h1>
+        <div className="row">
+          <div className="col-md-4">
+            <Newsitems title="Test" discription="Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti, alias!" url="https://youtu.be/dQw4w9WgXcQ" imgSrc="https://cdn.discordapp.com/attachments/1043247407946809438/1148180692195356794/F2SsrV1boAE2a05.png" />
+          </div>
+          <div className="col-md-4">
+            <Newsitems title="Test" discription="Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti, alias!" url="https://youtu.be/dQw4w9WgXcQ" imgSrc="https://cdn.discordapp.com/attachments/1043247407946809438/1148180692195356794/F2SsrV1boAE2a05.png" />
+          </div>
+          <div className="col-md-4">
+            <Newsitems title="Test" discription="Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti, alias!" url="https://youtu.be/dQw4w9WgXcQ" imgSrc="https://cdn.discordapp.com/attachments/1043247407946809438/1148180692195356794/F2SsrV1boAE2a05.png" />
+          </div>
+          <div className="col-md-4">
+            <Newsitems title="Test" discription="Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti, alias!" url="https://youtu.be/dQw4w9WgXcQ" imgSrc="https://cdn.discordapp.com/attachments/1043247407946809438/1148180692195356794/F2SsrV1boAE2a05.png" />
+          </div>
+        </div>
       </div>
     )
   }
--- src/components/Newsitems.js
+++ src/components/Newsitems.js
@@ -2,9 +2,19 @@ import React, { Component } from 'react'
 
 export class Newsitems extends Component {
   render() {
+    let {title, description, url, imgSrc} = this.props;
     return (
       <div>
-        Item
+        <div className="card" style={{width: "18rem"}}>
+          {imgSrc && 
+            <img src={imgSrc} className="card-img-top" alt="..."/>
+          }
+          <div className="card-body">
+            <h5 className="card-title">{title}</h5>
+            <p className="card-text">{description}</p>
+            <a href={url} className="btn btn-primary">Source</a>
+          </div>
+        </div>
       </div>
     )
   }
