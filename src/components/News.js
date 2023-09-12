b841f4886c389b36391a8c2d9a467860e5a628a5
sdshan8 <sdshan8@gmail.com>
2023-09-12 20:59:22 +0530

    #27 done

--- src/components/News.js
+++ src/components/News.js
@@ -1,24 +1,39 @@
 import React, { Component } from 'react'
 import Newsitems from './Newsitems'
+import sample from '../lib/sample.json'
 
 export class News extends Component {
+  constructor(){
+    super()
+    this.state = {
+      articles: sample.articles,
+      loading: false
+    }
+  }
+  async componentDidMount(){
+      console.log("cdm");
+      let url = `https://newsapi.org/v2/top-headlines?country=in&apikey=${process.env.REACT_APP_NEWSAPI_KEY}`;
+      let data = await fetch(url);
+      let parsedData = await data.json()
+      console.log(parsedData);
+      this.setState({ articles: parsedData.articles })
+  }
   render() {
     return (
-      <div className='container my-3'>
+      <div className='container'>
         <h1>NewsMonkey - Top Headlines</h1>
         <div className="row">
-          <div className="col-md-4">
-            <Newsitems title="Test" discription="Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti, alias!" url="https://youtu.be/dQw4w9WgXcQ" imgSrc="https://cdn.discordapp.com/attachments/1043247407946809438/1148180692195356794/F2SsrV1boAE2a05.png" />
-          </div>
-          <div className="col-md-4">
-            <Newsitems title="Test" discription="Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti, alias!" url="https://youtu.be/dQw4w9WgXcQ" imgSrc="https://cdn.discordapp.com/attachments/1043247407946809438/1148180692195356794/F2SsrV1boAE2a05.png" />
-          </div>
-          <div className="col-md-4">
-            <Newsitems title="Test" discription="Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti, alias!" url="https://youtu.be/dQw4w9WgXcQ" imgSrc="https://cdn.discordapp.com/attachments/1043247407946809438/1148180692195356794/F2SsrV1boAE2a05.png" />
-          </div>
-          <div className="col-md-4">
-            <Newsitems title="Test" discription="Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti, alias!" url="https://youtu.be/dQw4w9WgXcQ" imgSrc="https://cdn.discordapp.com/attachments/1043247407946809438/1148180692195356794/F2SsrV1boAE2a05.png" />
-          </div>
+          {this.state.articles.map((element) => {
+            if (element.description) {
+              return (
+                <div key={element.url} className="col-md-3">
+                  <Newsitems title={element.title} description={element.description} imageUrl={element.urlToImage} newsUrl={element.url} />
+                </div>
+              )
+            } else {
+              return null
+            }
+          })}
         </div>
       </div>
     )
--- src/components/Newsitems.js
+++ src/components/Newsitems.js
@@ -2,17 +2,15 @@ import React, { Component } from 'react'
 
 export class Newsitems extends Component {
   render() {
-    let {title, description, url, imgSrc} = this.props;
+    let {title, description, imageUrl, newsUrl} = this.props;
     return (
-      <div>
+      <div className='my-3'>
         <div className="card" style={{width: "18rem"}}>
-          {imgSrc && 
-            <img src={imgSrc} className="card-img-top" alt="..."/>
-          }
+            <img src={!imageUrl?"https://fdn.gsmarena.com/imgroot/news/21/08/xiaomi-smart-home-india-announcements/-476x249w4/gsmarena_00.jpg":imageUrl} className="card-img-top" alt="..."/>
           <div className="card-body">
-            <h5 className="card-title">{title}</h5>
-            <p className="card-text">{description}</p>
-            <a href={url} className="btn btn-primary">Source</a>
+            <h5 className="card-title">{title ? title.slice(0, 45)+"..." : null}</h5>
+            <p className="card-text">{description ? description.slice(0, 127)+"..." : null}</p>
+            <a href={newsUrl} target='_blank' rel='noreferrer' className="btn btn-sm btn-primary">Read More</a>
           </div>
         </div>
       </div>
