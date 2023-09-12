ab6f25caad705645c58aeafad101c819b655e46f
sdshan8 <sdshan8@gmail.com>
2023-09-12 21:22:38 +0530

    #28 done

--- src/components/News.js
+++ src/components/News.js
@@ -7,7 +7,8 @@ export class News extends Component {
     super()
     this.state = {
       articles: sample.articles,
-      loading: false
+      loading: false,
+      page: 1
     }
   }
   async componentDidMount(){
@@ -17,6 +18,31 @@ export class News extends Component {
       let parsedData = await data.json()
       console.log(parsedData);
       this.setState({ articles: parsedData.articles })
+  }
+  handleNextClick = async () => {
+    console.log("Next")
+    if (this.state.page + 1 > Math.ceil(this.state.articles.totalResults/20)) {
+    } else {
+        let url = `https://newsapi.org/v2/top-headlines?country=in&apikey=${process.env.REACT_APP_NEWSAPI_KEY}&page=${this.state.page + 1}&pageSize=20`;
+        let data = await fetch(url);
+        let parsedData = await data.json()
+        console.log(parsedData);
+        this.setState({
+            page: this.state.page + 1,
+            articles: parsedData.articles
+        })
+    }
+  } 
+  handlePrevClick = async() => {
+    console.log("Previous");
+    let url = `https://newsapi.org/v2/top-headlines?country=in&apikey=${process.env.REACT_APP_NEWSAPI_KEY}&page=${this.state.page - 1}&pageSize=20`;
+    let data = await fetch(url);
+    let parsedData = await data.json()
+    console.log(parsedData);
+    this.setState({
+        page: this.state.page - 1,
+        articles: parsedData.articles
+    })
   }
   render() {
     return (
@@ -34,6 +60,12 @@ export class News extends Component {
               return null
             }
           })}
+        </div>
+        <div className="container d-flex justify-content-between">
+          <button disabled={this.state.page<=1} type="button" class="btn btn-dark" onClick={this.handlePrevClick}> &larr;
+        Previous</button>
+          {this.state.page}
+          <button type="button" class="btn btn-dark" onClick={this.handleNextClick}>Next &rarr;</button>
         </div>
       </div>
     )
